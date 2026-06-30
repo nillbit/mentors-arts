aae43d458968aab0b08bb0d2c41288d1be730df5
nillbit <nillbit2000@gmail.com>
2026-06-30 13:41:10 +0300

    first commit

--- pages/+config.ts
+++ pages/+config.ts
@@ -5,7 +5,7 @@ import logo from "./assets/logo.svg";
 const config: Config = {
   title: "Mentors Arts  Centre  ",
   description: "Home of creativity",
-
+prerender:true,
   extends: [vikeReact],
 };
 
